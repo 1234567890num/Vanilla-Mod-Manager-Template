af207629b3e6b7fb201ad52ea99937199245ebf4
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-02-09 08:51:05 +0700

    Update Search.py

--- Search.py
+++ Search.py
@@ -2,17 +2,17 @@ import os
 import json
 import random
 
-#Get new music filenames
 sfiles = []
 folder1 = 'ard'
 for root, dirs, files in os.walk(folder1):
     for file in files:
         sfiles.append(root+'\\'+file)
 
+Text = 'SetJump Type 0'
 print('Searching ...')
-Text = 'Type '
 for file in sfiles:
     f = open(file).read()
     if Text in f:
         x = f.find(Text)
-        print(file, f[x:x+7].strip())
+        x = ''
+        print(file, x)
