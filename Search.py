9626fd3c60451b8bb3ae602e50ae36f754bf45c4
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-03-27 21:32:02 +0700

    TT flag re-definition

--- Search.py
+++ Search.py
@@ -8,11 +8,11 @@ for root, dirs, files in os.walk(folder1):
     for file in files:
         sfiles.append(root+'\\'+file)
 
-Text = 'Unk1d'
+Text = 'SetProgressFlag 0x106F'
 print('Searching for '+Text+'...')
 for file in sfiles:
     f = open(file).read()
-    if Text in f and ('Unk06' in f or 'BattleLevel' in f):
+    if Text in f:
         x = f.find(Text)
         x = ''
         print(file, x)
